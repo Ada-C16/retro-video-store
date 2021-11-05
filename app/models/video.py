5e60a660055075340f8ee8ea3460a2fcb5be11f4
Kristel Jabbusch <kaj12@my.fsu.edu>
2021-11-04 18:17:01 -0700

    adding video attributes

--- app/models/video.py
+++ app/models/video.py
@@ -2,3 +2,6 @@ from app import db
 
 class Video(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    release_date = db.Column(db.DateTime, nullable = False)
+    total_inventory = db.Column(db.Integer, nullable = False)
+    
