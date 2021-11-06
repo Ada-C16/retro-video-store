788ce55fc860bb34aa38d019ea3ce3f581a7d153
Rachael <mcbride.rac@gmail.com>
2021-11-06 14:42:14 -0400

    Adds delete_customer and update_customer routes; passes customer part of wave #1

--- app/models/customer.py
+++ app/models/customer.py
@@ -4,17 +4,15 @@ class Customer(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String, nullable=False)
     postal_code = db.Column(db.String, nullable=False)
-    phone_number = db.Column(db.String, nullable=False)
+    phone = db.Column(db.String, nullable=False)
     register_at = db.Column(db.DateTime) 
     # videos = db.relationship("Video", backref="customer", lazy=True)
 
-# reason for str = many zip codes start with a zero; if you store as int you will lose leading zero
-
-    def to_json(self, postal_code=None, phone_number=None):
+    def to_json(self):
         return {
             "id" : self.id,
             "name" : self.name,
             "postal_code" : self.postal_code,
-            "phone_number" : self.phone_number,
+            "phone" : self.phone,
             "register_at" : self.register_at
         }
--- app/routes.py
+++ app/routes.py
@@ -14,14 +14,17 @@ def customers():
 @customers_bp.route("", methods=["POST"])
 def add_customer():
     request_body = request.get_json()
-    if "name" not in request_body or "postal_code" not in request_body or\
-        "phone_number" not in request_body:
-        return { "Details" : "Invalid data" }, 400 
+    if "name" not in request_body:
+        return jsonify({ "details" : "Request body must include name." }), 400
+    if "postal_code" not in request_body:
+        return jsonify({ "details" : "Request body must include postal_code." }), 400 
+    if "phone" not in request_body:
+        return jsonify({ "details" : "Request body must include phone." }), 400 
     
     new_customer = Customer(
         name=request_body["name"], 
         postal_code=request_body["postal_code"],
-        phone_number=request_body["phone_number"],
+        phone=request_body["phone"],
         register_at = datetime.now()
     )
     
@@ -30,3 +33,43 @@ def add_customer():
     
     return jsonify({ "customer" : new_customer.to_json() }), 201
 
+@customers_bp.route("/<customer_id>", methods=["GET"])
+def customer(customer_id):
+    if not customer_id.isnumeric():
+        return "", 400 
+    
+    customer = Customer.query.get(customer_id)
+    if not customer:
+        return {"message": f"Customer {customer_id} was not found"}, 404 
+
+    return jsonify(customer.to_json()), 200
+
+@customers_bp.route("/<customer_id>", methods=["PUT"])
+def update_customer(customer_id):
+    customer = Customer.query.get(customer_id)
+    if customer is None:
+        return make_response(
+            {"message": f"Customer {customer_id} was not found"}, 404)
+
+    request_data = request.get_json()
+    for required_category in ["name", "phone", "postal_code"]:
+        if required_category not in request_data: 
+            return "", 400 
+
+    customer.name = request_data["name"]
+    customer.phone = request_data["phone"]
+    customer.postal_code = request_data["postal_code"]
+    db.session.commit()
+
+    return jsonify(customer.to_json()), 200 
+
+@customers_bp.route("/<customer_id>", methods=["DELETE"])
+def delete_customer(customer_id):
+    customer = Customer.query.get(customer_id)
+    if not customer:
+        return {"message": f"Customer {customer_id} was not found"}, 404 
+    
+    db.session.delete(customer)
+    db.session.commit()
+
+    return jsonify({ "id": int(customer_id) }), 200 
