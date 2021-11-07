c30c712c19c27e1b87d1b084478922459afe2129
Genie <geniearcila@gmail.com>
2021-11-07 14:59:35 -0500

    passes 2 more tests

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,11 @@
 from app import db 
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request, make_response 
 from app.models.customer import Customer
 from app.models.video import Video
 from datetime import datetime
 import requests
+import os
+from dotenv import load_dotenv
 
 customers_bp = Blueprint("customers",__name__,url_prefix = "/customers") # path that gives you access to resources/endpoint
 videos_bp = Blueprint("videos",__name__,url_prefix = "/videos")
@@ -11,7 +13,6 @@ videos_bp = Blueprint("videos",__name__,url_prefix = "/videos")
 @videos_bp.route("", methods=["GET"])
 def handle_videos():
 
-    
     videos_table = Video.query.all() # searching for all the videos in the videos table
     videos_list_response = [] # empty list 
     # if videos_table is None:
@@ -28,15 +29,26 @@ def handle_videos():
                 "release_date": False,
                 "total_inventory": each_video.total_inventory
 
-
-
-
             }
         )
     print (videos_list_response)
     return jsonify(videos_list_response), 200
 
+@videos_bp.route("/<video_id>", methods=["GET"])
+def handle_video(video_id):
+    videos_table = Video.query.get(video_id)
+    if videos_table is None:
+        return {"message": "Video 1 was not found"}, 404
+    elif request.method == "GET":
+        return {
+            "id": videos_table.id,
+            "title": videos_table.title,
+            "release_date": videos_table.release_date,
+            "total_inventory": videos_table.total_inventory
+        }, 200
 
+
+        
 
 
 
