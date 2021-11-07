992727679430d7638661b62ad75fcd3edbd2cfd6
ayaka <ayakaob@gmail.com>
2021-11-07 15:16:34 -0800

    create Video model

--- app/models/customer.py
+++ app/models/customer.py
@@ -4,4 +4,5 @@ class Customer(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(40))
     postal_code = db.Column(db.String(10))
+    phone_number = db.Column(db.String)
     register_at = db.Column(db.DateTime)
