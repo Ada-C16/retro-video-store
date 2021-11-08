54ead944d0e0cadf452372410350ddb3340cd4f9
KitSutliff <38538497+KitSutliff@users.noreply.github.com>
2021-11-08 11:34:15 -0800

    adds video model into app

--- app/models/video.py
+++ app/models/video.py
@@ -1,4 +1,21 @@
 from app import db
 
 class Video(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    video_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String(100))
+    release_date = db.Column(db.DateTime)
+    total_inventory = db.Column(db.Integer, nullable=True)
+    # customer_id = db.Column(db.Integer, db.ForeignKey(
+    #     "customer.customer_id"), nullable=True)
+
+    def to_dict(self):
+        """converts task data to dictionary"""
+        result = {"video_id": self.video_id,
+                  "title": self.title,
+                  "release_date": self.release_date,
+                  "total_inventory": self.total_inventory
+                  }
+
+        # if self.customer_id:
+        #     result["customer_id"] = self.customer_id
+        return result
