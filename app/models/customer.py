224bb69f6ebcbbf5a5a2c33915040ca0ebbc90e5
sarahstandish <z.sisyphusrocks@gmail.com>
2021-11-08 15:56:33 -0800

    add PGEt and PUT routes for /customer/id

--- app/models/customer.py
+++ app/models/customer.py
@@ -4,9 +4,9 @@ from datetime import datetime
 class Customer(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(200))
-    postal_code = db.Column(db.String(10)) # Up to 10 chars in case using extended postal code
+    postal_code = db.Column(db.String(50))
     registered_at = db.Column(db.DateTime)
-    phone = db.Column(db.String(30))
+    phone = db.Column(db.String(50))
     __tablename__ = "customers"
 
     def to_dict(self):
@@ -21,9 +21,9 @@ class Customer(db.Model):
     @classmethod
     def from_json(cls, request_body):
         return cls(
-            name = request_body["name"],
-            postal_code = request_body["postal_code"],
-            phone = request_body["phone"],
+            name = str(request_body["name"]),
+            postal_code = str(request_body["postal_code"]),
+            phone = str(request_body["phone"]),
             registered_at = datetime.now()
         )
 
@@ -47,5 +47,11 @@ class Customer(db.Model):
         required_fields = ["name", "postal_code", "phone"]
 
         for field in required_fields:
-            if field not in request_body or not isinstance(request_body[field], str):
-                return { "details" : f"Request body must include {field}"}, 400
+            if field not in request_body:
+                return { "details" : f"Request body must include {field}."}, 400
+
+        if len(request_body["postal_code"]) > 50 or len(request_body["phone"]) > 50 or len(request_body["name"]) > 200:
+            return {
+                "details" : "Input length exceeds database capacity."
+            }, 400
+
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 import re
 from flask.blueprints import Blueprint
+from sqlalchemy.sql.operators import custom_op
 from app import db
 from flask import Flask, jsonify, request
 from .models.customer import Customer
@@ -31,3 +32,37 @@ def handle_customers():
         customers = Customer.query.all()
 
         return jsonify([customer.to_dict() for customer in customers]), 200
+
+@customer_bp.route("/<cust_id>", methods=["GET", "PUT", "DELETE"])
+def handle_customer(cust_id):
+
+    id_error = Customer.validate_id(cust_id)
+
+    if id_error:
+        return id_error
+
+    customer = Customer.query.get(cust_id)
+    
+    if request.method == "GET":
+        
+        return customer.to_dict(), 200
+
+    elif request.method == "PUT":
+
+        request_body = request.get_json()
+
+        input_error = Customer.check_input_fields(request_body)
+
+        if input_error:
+            return input_error 
+
+        customer.name = request_body["name"]
+        customer.postal_code = request_body["postal_code"]
+        customer.phone = request_body["phone"]
+
+        db.session.commit()
+
+        return customer.to_dict(), 200
+
+    elif request.method == "DELETE":
+        pass
