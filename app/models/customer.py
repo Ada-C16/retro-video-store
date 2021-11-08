b958d961ee680a6b19bb8ac3809205e52ee6c5f7
jacy <jacyyang04@gmail.com>
2021-11-08 11:54:45 -0800

    add datetime default

--- app/models/customer.py
+++ app/models/customer.py
@@ -1,11 +1,12 @@
 from app import db
+from datetime import datetime
 
 class Customer(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     phone = db.Column(db.String)
     postcode = db.Column(db.Integer)
-    register_at = db.Column(db.DateTime)
+    register_at = db.Column(db.DateTime, default=datetime.now)
 
     def to_dict(self):
         new_dict = {
