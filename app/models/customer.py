e85b27f2ab989e4ff105b387910d67c186fe4029
jacy <jacyyang04@gmail.com>
2021-11-08 11:35:45 -0800

    add customer class

--- app/models/customer.py
+++ app/models/customer.py
@@ -2,3 +2,16 @@ from app import db
 
 class Customer(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    phone = db.Column(db.String)
+    postcode = db.Column(db.Integer)
+    register_at = db.Column(db.DateTime)
+
+    def to_dict(self):
+        new_dict = {
+            "name": self.name,
+            "phone": self.phone,
+            "postcode": self.postcode
+        }
+        
+        return new_dict
