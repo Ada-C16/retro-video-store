245154bcb9dc0a0c11e5a2fb06a74bcdc892f956
Laurel S <ls60@protonmail.com>
2021-11-08 12:02:48 -0800

    Create get route for /videos and import flask modules

--- app/routes/video_routes.py
+++ app/routes/video_routes.py
@@ -1,4 +1,6 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
+from flask.helpers import make_response
+from app.models.video import Video
 # GET /videos
 # GET /videos/<id>
 # POST /videos
@@ -6,3 +8,9 @@ from flask import Blueprint
 # DELETE /videos/<id>
 
 videos_bp = Blueprint('videos', __name__, url_prefix='/videos')
+
+@videos_bp.route('', methods=['GET'], strict_slashes=False)
+def get_all_videos():
+    videos = Video.query.all()
+    video_list =  [video.to_dict() for video in videos]
+    return make_response(jsonify(video_list), 200)
