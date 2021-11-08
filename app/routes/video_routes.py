6f966f0d260ac87951769d1de4cda98cb4a4417f
Laurel S <ls60@protonmail.com>
2021-11-08 12:48:17 -0800

    Add create video route which requires valid request body

--- app/routes/video_routes.py
+++ app/routes/video_routes.py
@@ -1,6 +1,7 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 from flask.helpers import make_response
 from app.models.video import Video
+from app import db
 # GET /videos
 # GET /videos/<id>
 # POST /videos
@@ -14,3 +15,24 @@ def get_all_videos():
     videos = Video.query.all()
     video_list =  [video.to_dict() for video in videos]
     return make_response(jsonify(video_list), 200)
+
+@videos_bp.route('', methods=['POST'], strict_slashes=False)
+def create_video():
+    request_body = request.get_json()
+    missing_fields = []
+    if 'title' not in request_body:
+        missing_fields.append('Request body must include title.')
+    if 'release_date' not in request_body:
+        missing_fields.append('Request body must include release_date.')
+    if 'total_inventory' not in request_body:
+        missing_fields.append('Request body must include total_inventory.')
+    if missing_fields:
+        return make_response({'details': missing_fields}, 400)
+    new_video = Video(
+        title = request_body['title'],
+        release_date = request_body['release_date'],
+        total_inventory = request_body['total_inventory']
+    )
+    db.session.add(new_video)
+    db.session.commit()
+    return make_response(new_video.to_dict(), 201)
