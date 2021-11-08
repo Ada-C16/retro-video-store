7b4726141a7a96db57c5f7a02e1ccc394400d768
brew doctor <>
2021-11-08 23:11:17 +0400

    created video route

--- app/__init__.py
+++ app/__init__.py
@@ -32,7 +32,7 @@ def create_app(test_config=None):
     migrate.init_app(app, db)
 
     #Register Blueprints Here
-    from .routes import videos_bp
+    from .video_route import videos_bp
     app.register_blueprint(videos_bp)
 
     return app
