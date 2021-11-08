607b774664ee853b114a65c0bb87c86f4f062df9
Mariah Rosman <mrosman7@gmail.com>
2021-11-08 11:04:04 -0800

    instatiates Customer Model

--- app/models/customer.py
+++ app/models/customer.py
@@ -1,4 +1,8 @@
 from app import db
 
 class Customer(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String, nullable=False)
+    postal_code = db.Column(db.Integer, nullable=True)
+    phone_num = db.Column(db.String, nullable=False)
+    register_at = db.Column(db.DateTime, nullable=False)
