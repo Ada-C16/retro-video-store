3c59c31be8a734673d9e4abbfe81b98ec36687b6
ayaka <ayakaob@gmail.com>
2021-11-07 16:28:31 -0800

    routes DELETE & PUT /videos/<video_id>

--- app/routes.py
+++ app/routes.py
@@ -14,6 +14,26 @@ def get_all_videos():
     response_body = [video.to_dict() for video in videos]
     return jsonify(response_body)
 
+@videos_bp.route("", methods=["POST"])
+def create_video():
+    request_body = request.get_json()
+
+    if "title" not in request_body:
+        return {"details": "Request body must include title."}, 400
+    if "release_date" not in request_body:
+        return {"details": "Request body must include release_date."}, 400
+    if "total_inventory" not in request_body:
+        return {"details": "Request body must include total_inventory."}, 400
+
+    new_video = Video(
+        title= request_body["title"],
+        release_date= request_body["release_date"],
+        total_inventory= request_body["total_inventory"]
+    )
+    db.session.add(new_video)
+    db.session.commit()
+
+    return jsonify(new_video.to_dict()), 201
 
 @videos_bp.route("/<video_id>", methods=["GET"])
 def get_video(video_id):
@@ -24,5 +44,41 @@ def get_video(video_id):
 
     if video is None:
         return jsonify({"message": f"Video {video_id} was not found"}), 404
+
+    return video.to_dict()
+
+@videos_bp.route("/<video_id>", methods=["DELETE"])
+def delete_video(video_id):
+    video = Video.query.get(video_id)
+
+    if video is None:
+        return {"message": f"Video {video_id} was not found"}, 404
+
+    db.session.delete(video)
+    db.session.commit()
+
+    return video.to_dict()
+
+@videos_bp.route("/<video_id>", methods=["PUT"])
+def update_video(video_id):
+    request_body = request.get_json()
+    video = Video.query.get(video_id)
+
+    if video is None:
+        return {"message": f"Video {video_id} was not found"}, 404
+
+    if "title" not in request_body:
+        return {"details": "Request body must include title."}, 400
+    if "release_date" not in request_body:
+        return {"details": "Request body must include release_date."}, 400
+    if "total_inventory" not in request_body:
+        return {"details": "Request body must include total_inventory."}, 400
+
+
+    video.title = request_body["title"]
+    video.total_inventory = request_body["total_inventory"]
+    video.release_date = request_body["release_date"]
+
+    db.session.commit()
 
     return video.to_dict()
