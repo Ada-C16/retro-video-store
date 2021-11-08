eb522c577acab3b04660c3bc6c41a1f1b44f2a85
jacy <jacyyang04@gmail.com>
2021-11-08 12:42:18 -0800

    update postalcode datatype

--- app/models/customer.py
+++ app/models/customer.py
@@ -5,7 +5,7 @@ class Customer(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     phone = db.Column(db.String)
-    postal_code = db.Column(db.Integer)
+    postal_code = db.Column(db.String)
     register_at = db.Column(db.DateTime, default=datetime.now)
 
     def to_dict(self):
