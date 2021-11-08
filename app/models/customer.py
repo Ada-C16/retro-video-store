d44cfca7cf0b3159f926995b651be27f9434988b
Lizet Tovar <lizett503@gmail.com>
2021-11-08 11:30:17 -0800

    Create customer and video models.

--- app/models/customer.py
+++ app/models/customer.py
@@ -2,3 +2,17 @@ from app import db
 
 class Customer(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    name=db.Column(db.String)
+    postal_code=db.Column(db.Integer)
+    phone=db.Column(db.Integer)
+    registered_at=db.Column(db.DateTime)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "registered_at": self.registered_at,
+            "postal_code": self.postal_code,
+            "phone": self.phone
+        }
+   
