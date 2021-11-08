ada7c55ec475569e3ceab7ac1718eda62270e5f2
Laurel S <ls60@protonmail.com>
2021-11-08 12:14:01 -0800

    Rename video table column to 'total_inventory'

--- app/models/video.py
+++ app/models/video.py
@@ -4,12 +4,12 @@ class Video(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String)
     release_date = db.Column(db.DateTime)
-    inventory = db.Column(db.Integer)
+    total_inventory = db.Column(db.Integer)
 
     def to_dict(self):
         return {
             "id": self.id,
             "title": self.title,
             "release_date": self.release_date,
-            "inventory": self.inventory
+            "total_inventory": self.total_inventory
         }
