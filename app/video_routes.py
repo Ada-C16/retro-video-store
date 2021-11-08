be5057b3e981ecd4a90171ed62e50beb75f15808
mac-madison <macanmadison@gmail.com>
2021-11-08 11:29:48 -0800

    add validation and GET

--- app/video_routes.py
+++ app/video_routes.py
@@ -3,3 +3,26 @@ from app import db
 from app.models.video import Video
 
 videos_bp = Blueprint("videos", __name__, url_prefix="/videos")
+
+
+def validate(id):
+
+    try:
+        id = int(id)
+    except ValueError:
+        abort(400)
+    return Video.query.get_or_404(id)
+
+
+@videos_bp.route("", methods=["GET"])
+def get_all():
+
+    videos = Video.query.all()
+    return jsonify([video.to_dict() for video in videos])
+
+
+@videos_bp.route("", methods=["GET"])
+def get_one(id):
+
+    video = validate(id)
+    return video.to_dict()
