2affd690e3c69096ed9f7e938efb0e3aef1a7058
mac-madison <macanmadison@gmail.com>
2021-11-08 12:23:54 -0800

    add helper methods

--- app/video_routes.py
+++ app/video_routes.py
@@ -1,17 +1,25 @@
-from flask import Blueprint, jsonify, request, abort
+from flask import Blueprint, jsonify, request, abort, make_response
 from app import db
 from app.models.video import Video
 
 videos_bp = Blueprint("videos", __name__, url_prefix="/videos")
 
 
-def validate(id):
+def valid_id(id):
 
     try:
         id = int(id)
     except ValueError:
         abort(400)
-    return Video.query.get_or_404(id)
+    return Video.query.get(id)
+
+
+def get_video(id):
+
+    video = valid_id(id)
+    if not video:
+        abort(make_response({"message": f"Video {id} was not found"}, 404))
+    return video
 
 
 @videos_bp.route("", methods=["GET"])
@@ -24,5 +32,5 @@ def get_all():
 @videos_bp.route("/<id>", methods=["GET"])
 def get_one(id):
 
-    video = validate(id)
+    video = get_video(id)
     return video.to_dict()
