65a1904953f9ecbfeb7d3c65fc94143d0c958556
sarahstandish <z.sisyphusrocks@gmail.com>
2021-11-08 11:14:25 -0800

    add customer table

--- app/models/customer.py
+++ app/models/customer.py
@@ -1,4 +1,12 @@
 from app import db
 
 class Customer(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(200))
+    postal_code = db.Column(db.Integer)
+    registered_at = db.Column(db.DateTime)
+    phone = db.Column(db.String(30))
+    __tablename__ = "customers"
+
+    def to_dict(self):
+        pass
