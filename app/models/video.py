916aed5cc96088835bed442cd08283b0df0448e6
Laurel S <ls60@protonmail.com>
2021-11-08 12:02:06 -0800

    Add to_dict instance method to Video model

--- app/models/video.py
+++ app/models/video.py
@@ -5,3 +5,11 @@ class Video(db.Model):
     title = db.Column(db.String)
     release_date = db.Column(db.DateTime)
     inventory = db.Column(db.Integer)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "release_date": self.release_date,
+            "inventory": self.inventory
+        }
