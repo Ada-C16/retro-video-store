225a3b21d9e91e259f97474b904b53da8316ef33
jacy <jacyyang04@gmail.com>
2021-11-08 16:41:04 -0800

    add delete routes

--- app/routes/customer_routes.py
+++ app/routes/customer_routes.py
@@ -2,10 +2,11 @@ from app import db
 from app.models.customer import Customer
 from flask import Blueprint, jsonify, make_response, request, abort
 
-# Blueprint
+# BLUEPRINT
 customers_bp = Blueprint("customers_bp", __name__, url_prefix = "/customers")
 
-# Helper Functions
+
+# HELPER FUNCTIONS
 def validate_int(customer_id, param_type):
     try:
         number = int(customer_id)
@@ -14,7 +15,6 @@ def validate_int(customer_id, param_type):
 
 def get_customer_data_with_id(customer_id):
     validate_int(customer_id, "id")
-    # return Customer.query.get_or_404(customer_id, description={"message": f"Customer {customer_id} was not found"})
     customer = Customer.query.get(customer_id)
 
     if customer is None:
@@ -22,7 +22,8 @@ def get_customer_data_with_id(customer_id):
 
     return customer
 
-# Routes
+
+# GENERAL ROUTES
 @customers_bp.route("", methods=["GET"])
 def get_all_customers():
     customer_data = []
@@ -38,10 +39,14 @@ def create_customer():
     request_body = request.get_json()
 
     if request_body == None:
-        return make_response("You much include name, phone and postcode in order to add customer data."), 404
+        return make_response("You much include name, phone and postcode in order to add customer data.", 404)
 
-    if "name" not in request_body or "phone" not in request_body or "postcode" not in request_body:
-        return make_response("You much include name, phone and postcode in order to add customer data."), 404
+    if "name" not in request_body:
+        return make_response({"details":"Request body must include name."}, 400)
+    elif "phone" not in request_body:
+        return make_response({"details":"Request body must include phone."}, 400)
+    elif "postal_code" not in request_body:
+        return make_response({"details":"Request body must include postal_code."}, 400)
 
     new_customer = Customer(
         name = request_body["name"],
@@ -52,23 +57,34 @@ def create_customer():
     db.session.add(new_customer)
     db.session.commit()
 
-    return jsonify(new_customer), 200
+    return jsonify(new_customer.to_dict()), 201
 
 
+# CUSTOMER_ID ROUTES
 @customers_bp.route("/<customer_id>", methods=["GET"])
 def get_one_customer(customer_id):
     customer = get_customer_data_with_id(customer_id)
 
     return jsonify(customer.to_dict())
 
-# @customers_bp.route("/<customer_id>", methods=["PUT"])
-# def update_one_customer(customer_id):
-#     request_body = request.get_json()
-#     customer = get_customer_data_with_id(customer_id)
+@customers_bp.route("/<customer_id>", methods=["PUT"])
+def update_one_customer(customer_id):
+    request_body = request.get_json()
+    customer = get_customer_data_with_id(customer_id)
 
-#     if customer == None:
-#         error_message = {}
-#         error_message["message"] = f"Customer {customer_id} was not found"
-#         return error_message, 404
+    customer.name = request_body["name"]
+    customer.phone = request_body["phone"]
+    customer.postal_code = request_body["postal_code"]
 
-#     return jsonify(customer.to_dict())
+    return jsonify(customer.to_dict())
+
+
+# DELETE ROUTE
+@customers_bp.route("/<customer_id>", methods=["DELETE"])
+def delete_customer(customer_id):
+    customer = get_customer_data_with_id(customer_id)
+
+    db.session.delete(customer)
+    db.session.commit()
+
+    return make_response({"id": customer.id}, 200)
