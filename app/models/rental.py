ba94d12a8757caa40989796c50611c4c83ba873c
kaitlyngore <kaitlyngore@gmail.com>
2021-11-09 15:33:08 -0500

    added check-out route

--- app/models/rental.py
+++ app/models/rental.py
@@ -1,6 +1,8 @@
 from app import db
 from sqlalchemy.sql.functions import func
 import datetime
+from app.models.video import Video
+from app.models.customer import Customer
 
 
 class Rental(db.Model):
@@ -12,6 +14,18 @@ class Rental(db.Model):
         'customer.customer_id'), primary_key=True, nullable=False)
     due_date = db.Column(db.DateTime, nullable=False,
                          default=datetime.datetime.utcnow() + datetime.timedelta(days=7))
-                    
+
     # customer = db.relationship("Customer", backref="rentals")
     # video = db.relationship("Video", backref="rentals")
+    def rental_dict(self):
+        video = Video.query.get(self.video_id)
+        available_inventory = video.total_inventory
+
+        customer = Customer.query.get(self.customer_id)
+        videos_checked_out = customer.videos_checked_out
+        return {
+            "video_id": self.video_id,
+            "customer_id": self.customer_id,
+            "videos_checked_out_count": videos_checked_out,
+            "available_inventory": available_inventory
+        }
--- app/rental_routes.py
+++ app/rental_routes.py
@@ -4,3 +4,41 @@ from app.models.video import Video
 from app.models.customer import Customer
 from app.models.rental import Rental
 
+rentals_bp = Blueprint("rentals_bp", __name__, url_prefix="/rentals")
+
+
+@rentals_bp.route("/check-out", methods=["POST"])
+def check_out():
+    request_body = request.get_json()
+    if "customer_id" not in request_body:
+        response_body = {"details": "Request body must include customer_id."}
+        return jsonify(response_body), 400
+    elif "video_id" not in request_body:
+        response_body = {"details": "Request body must include video_id."}
+        return jsonify(response_body), 400
+
+    video_id = request_body["video_id"]
+    customer_id = request_body["customer_id"]
+    video = Video.query.get(video_id)
+    if not video:
+        return jsonify(None), 400
+    if video.total_inventory == 0:
+        return jsonify("Could not perform checkout"), 400
+    new_inventory_total = video.total_inventory - 1
+    video.total_inventory = new_inventory_total
+
+    customer = Customer.query.get(customer_id)
+    if not customer:
+        return jsonify(None), 400
+    videos_checked_out = customer.videos_checked_out - 1
+    customer.videos_checked_out = videos_checked_out
+
+    new_rental = Rental(
+        video_id=request_body["video_id"],
+        customer_id=request_body["customer_id"]
+    )
+
+    db.session.add(new_rental)
+    db.session.commit()
+    response_body = new_rental.rental_dict()
+    return jsonify(response_body), 201
