c21d634aeefaf7744257e2f19c1dcf396b70a96c
Kristel Jabbusch <kaj12@my.fsu.edu>
2021-11-09 15:24:58 -0800

    all video routes pass tests

--- app/video_routes.py
+++ app/video_routes.py
@@ -12,9 +12,13 @@ def handle_videos():
 
     elif request.method == 'POST':
         request_body = request.get_json()
+        
         #checks for list if we want to post multiple videos
         if isinstance(request_body, list):
             for video in request_body:
+                if validate_video(video) != True:
+                    return validate_video(video)
+                
                 new_video = Video(
                     title = video['title'],
                     release_date = video['release_date'],
@@ -22,14 +26,19 @@ def handle_videos():
                 )
                 db.session.add(new_video)
                 return jsonify({"video": video.to_dict()} for video in request_body), 201
+        
         else:
+            if validate_video(request_body) != True:
+                return validate_video(request_body)
+            
             new_video = Video(
                 title = request_body['title'],
                 release_date = request_body['release_date'],
                 total_inventory = request_body['total_inventory']
             )
             db.session.add(new_video)
-            return jsonify({"video": new_video.to_dict()}), 201
+            db.session.commit()
+            return jsonify(new_video.to_dict()), 201
 
 @videos_bp.route('/<id_num>', methods=['GET', 'PUT', 'DELETE'])
 def handle_video(id_num):
@@ -47,18 +56,30 @@ def handle_video(id_num):
 
     elif request.method == 'PUT':
         request_body = request.get_json()
+        if validate_video(request_body) != True:
+            return validate_video(request_body)
+        
         for key, value in request_body.items():
             if key in Video.__table__.columns.keys():
                 setattr(video, key, value)
+                
         db.session.commit()
-        return jsonify({"video": video.to_dict()}), 200
+        return jsonify(video.to_dict()), 200
     
     elif request.method == 'DELETE':
-        video_id = video.to_dict()['id']
-        video_title = video.to_dict()['title']
-
         db.session.delete(video)
         db.session.commit()
-        return jsonify({"details": f"Video {video_title} with id {video_id} was deleted"}), 200
+        return jsonify({"id": video.id}), 200
 
 
+def validate_video(response_body):
+    if not response_body:
+        return make_response(jsonify({"error": "No data was sent"}), 400)
+    elif 'title' not in response_body:
+        return make_response(jsonify({"details": "Request body must include title."}), 400)
+    elif 'release_date' not in response_body:
+        return make_response(jsonify({"details": "Request body must include release_date."}), 400)
+    elif 'total_inventory' not in response_body:
+        return make_response(jsonify({"details": "Request body must include total_inventory."}), 400)
+    else:
+        return True
