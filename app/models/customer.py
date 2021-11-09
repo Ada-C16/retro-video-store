9da2f410a8e55098c6b92f4efd22ba283e1dc376
Marjan <marjan.abm@gmail.com>
2021-11-09 10:41:26 -0800

    customer model

--- app/models/customer.py
+++ app/models/customer.py
@@ -1,4 +1,13 @@
+from sqlalchemy.orm import backref
 from app import db
+from os import register_at_fork
+from datetime import datetime
 
 class Customer(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    __tablename__ = "customers"
+    customer_id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    postal_code = db.Column(db.String)
+    phone = db.Column(db.String)
+    registered_at = db.Column(db.DateTime, default=datetime.utcnow())
+    rentals = db.relationship('Rental',backref='customer', lazy=True)
