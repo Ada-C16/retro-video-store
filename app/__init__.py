306c7092097821824203686a30ab78ea5496d943
Kristel Jabbusch <kaj12@my.fsu.edu>
2021-11-09 12:01:29 -0800

    register blueprints

--- app/__init__.py
+++ app/__init__.py
@@ -33,5 +33,9 @@ def create_app(test_config=None):
     migrate.init_app(app, db)
 
     #Register Blueprints Here
+    from .customer_routes import customers_bp
+    from .rental_routes import videos_bp
+    app.register_blueprint(customers_bp)
+    app.register_blueprint(videos_bp)
 
     return app
