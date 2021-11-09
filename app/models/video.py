a8b495c964df97a7e7eca0ba1f7cac817504ed21
Kristel Jabbusch <kaj12@my.fsu.edu>
2021-11-09 11:37:38 -0800

    hiding video values not required

--- app/models/video.py
+++ app/models/video.py
@@ -1,15 +1,38 @@
 from app import db
 
 class Video(db.Model):
+    __tablename__ = 'videos'
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     release_date = db.Column(db.DateTime, nullable = False)
     total_inventory = db.Column(db.Integer, nullable = False)
 
     # additional attributes
-    genres = db.Column(db.String(100), nullable=False)
-    rating = db.Column(db.String(10), nullable=False)
-    user_rating = db.Column(db.Float(10), nullable=False)
-    runtime = db.Column(db.Integer(20), nullable=False)
-    description = db.Column(db.String(1000), nullable=False)
-    platforms = db.Column(db.String(1000), nullable=True)
+    # genres = db.Column(db.String(100), nullable=False)
+    # rating = db.Column(db.String(10), nullable=False)
+    # user_rating = db.Column(db.Float(10), nullable=False)
+    # runtime = db.Column(db.Integer, nullable=False)
+    # description = db.Column(db.String(1000), nullable=False)
+    # platforms = db.Column(db.String(1000), nullable=True)
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'title': self.title,
+            'release_date': self.release_date,
+            'total_inventory': self.total_inventory
+        }
+    
+    # def new_to_dict(self):
+    #     return {
+    #         'id': self.id,
+    #         'title': self.title,
+    #         'release_date': self.release_date,
+    #         'total_inventory': self.total_inventory,
+    #         'genres': self.genres,
+    #         'rating': self.rating,
+    #         'user_rating': self.user_rating,
+    #         'runtime': self.runtime,
+    #         'description': self.description,
+    #         'platforms': self.platforms
+    #     }
