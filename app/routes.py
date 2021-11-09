a364a7a2abae01c0260c97bf2c237426d4ce0217
Rae <rae.elwell@gmail.com>
2021-11-09 12:06:27 -0800

    adds GET video methods

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify, make_response, request
+from flask import Blueprint, jsonify, make_response, request, abort
 from app import db
 from app.models.customer import Customer
 from app.models.video import Video
@@ -9,6 +9,14 @@ import os
 #DEFINE BLUEPRINTS
 customer_bp = Blueprint("customers", __name__, url_prefix="/customers")
 video_bp = Blueprint("videos", __name__, url_prefix="/videos")
+
+#----------- HELPER FUNCTIONS -----------
+def validate_video_id(video_id):
+    try: 
+        video_id == int(video_id)
+    except:
+        abort(400, {"message": f"video {video_id} was not found"})
+    return Video.query.get_or_404(video_id)
 
 #----------- CREATE ---------------------
 @video_bp.route("", methods=["POST"])
@@ -32,3 +40,18 @@ def create_video():
     db.session.commit()
 
     return make_response(jsonify(new_video.to_dict()),201)
+
+#----------- GET ---------------------
+@video_bp.route("", methods=["GET"])
+def get_all_videos():
+    videos = Video.query.all()
+    videos_response=[]
+    for video in videos:
+        videos_response.append(video.to_dict())
+    return jsonify(videos_response)
+
+@video_bp.route("/<id>", methods=["GET"])
+def get_one_video(id):
+    video = validate_video_id(id)
+    request_body = request.get_json
+    return jsonify(video.to_dict())
