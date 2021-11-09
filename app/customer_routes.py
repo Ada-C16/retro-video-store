e4138b1cf258774a270ed99f46719f6aa4c9e5d8
Marjan <marjan.abm@gmail.com>
2021-11-09 11:40:27 -0800

    customer route

---
+++ app/customer_routes.py
@@ -0,0 +1,27 @@
+from app import db
+from app.models.customer import Customer
+from flask import Blueprint, json, jsonify, request, make_response
+import datetime
+
+customers_bp = Blueprint("customers",__name__, url_prefix="/customers")
+
+@customers_bp.route("",methods=["GET"])
+
+def get_customers():
+    customers = Customer.query.all()
+    customers_response = []
+    for customer in customers:
+        customers_response.append(customer.to_dict)
+
+    return jsonify(customers_response),200
+
+@customers_bp.route("/<customer_id>",methods=["GET"])
+def get_customer_by_id(customer_id):
+    customer = Customer.query.get_or_404(customer_id)
+    if not customer:
+        return make_response(jsonify({f"message": "Customer {customer_id} was not found"}),404)
+    return customer.to_dict(),200
+
+@customers_bp.route("",methods=["POST"])
+def new_customer():
+    request_body = request.get_json()
