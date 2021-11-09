a7b22604cd6c55fb4aed541bdf12cff8ef30de3f
Kristel Jabbusch <kaj12@my.fsu.edu>
2021-11-09 11:37:59 -0800

    video routes first draft

---
+++ app/video_routes.py
@@ -0,0 +1,59 @@
+from flask.helpers import total_seconds
+from app import db
+from app.models import Video
+from flask import Blueprint, jsonify, request, make_response
+
+videos_bp = Blueprint('videos', __name__, url_prefix="/videos")
+
+@videos_bp.route('/', methods=['GET', 'POST'])
+def handle_videos():
+    if request.method == 'GET':
+        videos = Video.query.all()
+        return jsonify([video.to_dict() for video in videos])
+    elif request.method == 'POST':
+        request_body = request.get_json()
+        #checks for list if we want to post multiple videos
+        if isinstance(request_body, list):
+            for video in request_body:
+                new_video = Video(
+                    title = video['title'],
+                    release_date = video['release_date'],
+                    total_inventory = video['total_inventory']
+                )
+                db.session.add(new_video)
+                return jsonify({"video": video.to_dict()} for video in request_body), 201
+        else:
+            new_video = Video(
+                title = request_body['title'],
+                release_date = request_body['release_date'],
+                total_inventory = request_body['total_inventory']
+            )
+            db.session.add(new_video)
+            return jsonify({"video": new_video.to_dict()}), 201
+
+@videos_bp.route('/<int:id_num>', methods=['GET', 'PUT', 'DELETE'])
+def handle_video(id_num):
+    video = Video.query.get_or_404(id_num)
+    if not video:
+        return make_response(jsonify({"message": "Video not found"}), 404)
+    
+    if request.method == 'GET':
+        return jsonify({"video": video.to_dict()}), 200
+
+    elif request.method == 'PUT':
+        request_body = request.get_json()
+        for key, value in request_body.items():
+            if key in Video.__table__.columns.keys():
+                setattr(video, key, value)
+        db.session.commit()
+        return jsonify({"video": video.to_dict()}), 200
+    
+    elif request.method == 'DELETE':
+        video_id = video.to_dict()['id']
+        video_title = video.to_dict()['title']
+
+        db.session.delete(video)
+        db.session.commit()
+        return jsonify({"details": f"Video {video_title} with id {video_id} was deleted"}), 200
+
+
