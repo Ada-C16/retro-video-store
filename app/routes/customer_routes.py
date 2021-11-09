7f280e1ed58cde0db3794e20afc15e4551ae3840
jacy <jacyyang04@gmail.com>
2021-11-08 16:02:47 -0800

    create get and post routes for all customers

--- app/routes/customer_routes.py
+++ app/routes/customer_routes.py
@@ -1,13 +1,26 @@
 from app import db
 from app.models.customer import Customer
-from flask import Blueprint, jsonify, make_response, request
+from flask import Blueprint, jsonify, make_response, request, abort
 
 # Blueprint
 customers_bp = Blueprint("customers_bp", __name__, url_prefix = "/customers")
 
 # Helper Functions
+def validate_int(customer_id, param_type):
+    try:
+        number = int(customer_id)
+    except:
+        abort(make_response({"message": f"{param_type} needs to be an integer."}, 400))
+
 def get_customer_data_with_id(customer_id):
-    return Customer.get_or_404(customer_id, descriptions={"details": "Invalid data"})
+    validate_int(customer_id, "id")
+    # return Customer.query.get_or_404(customer_id, description={"message": f"Customer {customer_id} was not found"})
+    customer = Customer.query.get(customer_id)
+
+    if customer is None:
+        abort(make_response({"message": f"Customer {customer_id} was not found"}, 404))
+
+    return customer
 
 # Routes
 @customers_bp.route("", methods=["GET"])
@@ -21,7 +34,7 @@ def get_all_customers():
     return jsonify(customer_data)
 
 @customers_bp.route("", methods=["POST"])
-def add_customer():
+def create_customer():
     request_body = request.get_json()
 
     if request_body == None:
@@ -40,3 +53,22 @@ def add_customer():
     db.session.commit()
 
     return jsonify(new_customer), 200
+
+
+@customers_bp.route("/<customer_id>", methods=["GET"])
+def get_one_customer(customer_id):
+    customer = get_customer_data_with_id(customer_id)
+
+    return jsonify(customer.to_dict())
+
+# @customers_bp.route("/<customer_id>", methods=["PUT"])
+# def update_one_customer(customer_id):
+#     request_body = request.get_json()
+#     customer = get_customer_data_with_id(customer_id)
+
+#     if customer == None:
+#         error_message = {}
+#         error_message["message"] = f"Customer {customer_id} was not found"
+#         return error_message, 404
+
+#     return jsonify(customer.to_dict())
