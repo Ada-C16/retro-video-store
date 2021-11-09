b41298fa01a9eb86ac3a027b071e29c36ed61270
Marjan <marjan.abm@gmail.com>
2021-11-09 11:39:46 -0800

    customer route

--- app/models/customer.py
+++ app/models/customer.py
@@ -10,4 +10,16 @@ class Customer(db.Model):
     postal_code = db.Column(db.String)
     phone = db.Column(db.String)
     registered_at = db.Column(db.DateTime, default=datetime.utcnow())
-    rentals = db.relationship('Rental',backref='customer', lazy=True)
+    # rentals = db.relationship('Rental',backref='customer', lazy=True)
+
+def to_dict(self):
+    result = {
+        "id":self.customer_id,
+        "name": self.name,
+        "postal_code": self.postal_code,
+        "phone":self.phone,
+        "registered_at": self.registered_at
+    }
+    # if self.rentals:
+    #     result["rentals"]=self.rentals
+    return result
