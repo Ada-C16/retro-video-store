1171adea449aefc5c86fb7255fa52f84049df09e
Kbhlee2121 <Kbhlee2121@gmail.com>
2021-11-09 02:09:12 -0800

    Create routes for GET, PUT, POST, DELETE

--- app/models/customer.py
+++ app/models/customer.py
@@ -3,8 +3,8 @@ from app import db
 class Customer(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String)
-    postal_code = db.Column(db.Integer)
-    phone_number = db.Column(db.String)
+    postal_code = db.Column(db.String)
+    phone = db.Column(db.String)
     registered_at = db.Column(db.DateTime, default=None, nullable=True)
 
     def to_dict(self):
@@ -13,5 +13,7 @@ class Customer(db.Model):
             "name": self.name,
             "registered_at": self.registered_at,
             "postal_code":self.postal_code,
-            "phone":self.phone_number,
+            "phone":self.phone,
         }
+
+    
