a9ae5849900012c90ec206b66f9027ce1baddcaf
Kristel Jabbusch <kaj12@my.fsu.edu>
2021-11-09 21:28:47 -0800

    check out route

--- app/rental_routes.py
+++ app/rental_routes.py
@@ -4,28 +4,39 @@ from flask import Blueprint, jsonify, request, make_response
 from app.models.customer import Customer
 from app.models.video import Video
 
-
 rentals_bp = Blueprint("rentals",__name__, url_prefix="/rentals")
+
 @rentals_bp.route("/<register>",methods=["GET","POST"])
 def handle_rental(register):
     if register == "check-in":
         pass
     elif register == "check-out":
         request_body = request.get_json()
+        if "customer_id" not in request_body or "video_id" not in request_body:
+            return make_response(jsonify({"error" : "Missing customer_id or video_id"}),400)
+        
         customer_id = request_body["customer_id"]
-        video_id = request_body["video_id"]
         customer = Customer.query.get(customer_id)
+        if not customer:
+            return make_response("Customer not found", 404)
+        
+        video_id = request_body["video_id"]
         video = Video.query.get(video_id)
+        if not video:
+            return make_response("Video not found", 404)
 
         rental = Rental(
             customer_id=customer.customer_id,
             video_id=video.video_id
         )
+        if video.gets_available_inventory() == 0:
+            return make_response({"message":"Could not perform checkout"}, 400)
+
         customer.rentals.append(rental)
         video.rentals.append(rental)
         
         customer.videos_checked_out_count += 1
-        video.available_inventory -= 1
+            
         db.session.commit()
         return make_response(jsonify(create_checkout_dict(video, customer)), 200) 
 
@@ -33,6 +44,5 @@ def create_checkout_dict(video, customer):
     result = {"video_id" : video.video_id,
     "customer_id" : customer.customer_id,
     "videos_checked_out_count" : customer.videos_checked_out_count,
-    "available_inventory" : video.available_inventory}
+    "available_inventory" : video.gets_available_inventory()}
     return result
-    
