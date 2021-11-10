bc971b848dafa5b9644e68f9ca48446246a6c26e
Citlalli I Zarate <37793116+MiffyBruna@users.noreply.github.com>
2021-11-10 14:08:01 -0800

    " customer_routes and customer.py pass wave 01"

--- app/models/customer.py
+++ app/models/customer.py
@@ -1,4 +1,19 @@
 from app import db
+from datetime import date
+
 
 class Customer(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement= True)
+    name = db.Column(db.String)
+    postal_code = db.Column(db.String)
+    phone = db.Column(db.String)
+    register_at = db.Column(db.DateTime, default= date.today())
+
+    def to_dict(self):
+        return{
+            "id": self.id,
+            "name": self.name,
+            "postal_code": self.postal_code,
+            "phone": self.phone,
+            "register_at": self.register_at
+        }
