b5a26e074c0f4ca8a0115c8651ec46d3cd2c0c8f
sarahstandish <z.sisyphusrocks@gmail.com>
2021-11-09 16:34:14 -0800

    add from_json method

--- app/models/rental.py
+++ app/models/rental.py
@@ -1,5 +1,5 @@
 from app import db
-from datetime import datetime
+from datetime import datetime, timedelta
 
 class Rental(db.Model):
     __tablename__ = "rentals"
@@ -8,12 +8,24 @@ class Rental(db.Model):
     video_id = db.Column(db.Integer, db.ForeignKey('videos.video_id'), primary_key=True, nullable=False)
     due_date = db.Column(db.DateTime)
     checked_out = db.Column(db.Boolean)
- 
+
     def to_dict(self):
         return {
             "id": self.id,
             "customer_id": self.customer_id,
             "video_id": self.video_id,
             "checked_out": self.checked_out,
-            "due_date": self.due_date.strftime("%a, %d %b %Y %H:%M:%S") if self.due_date else None
+            "due_date": self.due_date.strftime("%Y-%m-%d") if self.due_date else None
         }
+
+    @classmethod
+    def from_json(cls, customer_id, video_id):
+
+        now = datetime.now()
+
+        new_rental = cls(
+            customer_id = customer_id,
+            video_id = video_id,
+            checked_out = True,
+            due_date = now.timedelta(days=7)
+        )
