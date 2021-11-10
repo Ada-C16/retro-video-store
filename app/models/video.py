61b785cb5ea5d7603967615f9c98e15ced47659e
Kristel Jabbusch <kaj12@my.fsu.edu>
2021-11-09 21:29:47 -0800

    video changed available inventory to class method, not attribute

--- app/models/video.py
+++ app/models/video.py
@@ -6,36 +6,16 @@ class Video(db.Model):
     title = db.Column(db.String(100), nullable=False)
     release_date = db.Column(db.DateTime, nullable = False)
     total_inventory = db.Column(db.Integer, nullable = False)
-    available_inventory = db.Column(db.Integer, nullable = False)
     rentals = db.relationship('Rental', backref='videos', lazy=True)
-
-    # additional attributes
-    # genres = db.Column(db.String(100), nullable=False)
-    # rating = db.Column(db.String(10), nullable=False)
-    # user_rating = db.Column(db.Float(10), nullable=False)
-    # runtime = db.Column(db.Integer, nullable=False)
-    # description = db.Column(db.String(1000), nullable=False)
-    # platforms = db.Column(db.String(1000), nullable=True)
 
     def to_dict(self):
         return {
             'id': self.video_id,
             'title': self.title,
             'release_date': self.release_date,
-            'total_inventory': self.total_inventory,
-            'available_inventory': self.available_inventory
+            'total_inventory': self.total_inventory
         }
     
-    # def new_to_dict(self):
-    #     return {
-    #         'id': self.id,
-    #         'title': self.title,
-    #         'release_date': self.release_date,
-    #         'total_inventory': self.total_inventory,
-    #         'genres': self.genres,
-    #         'rating': self.rating,
-    #         'user_rating': self.user_rating,
-    #         'runtime': self.runtime,
-    #         'description': self.description,
-    #         'platforms': self.platforms
-    #     }
+    def gets_available_inventory(self):
+        available_inventory = self.total_inventory - len(self.rentals)
+        return available_inventory
