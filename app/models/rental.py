6a01510aca3c6efa6bf45609bdf1e245456aa8da
Kristel Jabbusch <kaj12@my.fsu.edu>
2021-11-09 21:29:14 -0800

    organized rental

--- app/models/rental.py
+++ app/models/rental.py
@@ -4,6 +4,8 @@ import datetime
 class Rental(db.Model):
     __tablename__ = 'rentals'
     rental_id = db.Column(db.Integer, primary_key=True)
+    due_date = db.Column(db.DateTime, default=datetime.datetime.now() + datetime.timedelta(days=7), nullable=False)
+    
     customer_id = db.Column(db.Integer, db.ForeignKey('customers.customer_id'), nullable=False)
     video_id = db.Column(db.Integer, db.ForeignKey('videos.video_id'), nullable=False)
-    due_date = db.Column(db.DateTime, default=datetime.datetime.now() + datetime.timedelta(days=7), nullable=False)
+    
