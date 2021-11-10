ce7116cc367de7e7d84c96ab319e21210bf0d5a8
Zandra Nguyen <zandra.eng@gmail.com>
2021-11-09 19:25:23 -0800

    made change to POST and GET methods. PUT is work in progress

--- app/customer_routes.py
+++ app/customer_routes.py
@@ -11,7 +11,14 @@ def customer_create():
     request_body = request.get_json()
 
     if "name" not in request_body or "phone" not in request_body or "postal_code" not in request_body:
-        return jsonify({"details": "Invalid data"}), 400
+        response_body ={}
+        if "name" not in request_body:
+            response_body["details"] = "Request body must include name."
+        elif "phone" not in request_body:
+            response_body["details"] = "Request body must include phone."
+        elif "postal_code" not in request_body:
+            response_body["details"] = "Request body must include postal_code."
+        return jsonify(response_body), 400
     
     new_customer = Customer(
         name=request_body["name"],
@@ -24,7 +31,8 @@ def customer_create():
 
     # response_body = {new_customer.customer_dict()}
     # return jsonify(response_body), 201
-    return jsonify({"id": new_customer.customer_id}), 201
+    # return jsonify({"id": new_customer.customer_id}), 201
+    return jsonify({"parameters": new_customer.customer_id}), 201
 
 
 @customers_bp.route("", methods=["GET"])
@@ -32,7 +40,7 @@ def handle_customers():
     customers = Customer.query.all()
     response_body = []
     for customer in customers:
-        response_body.append({customers.customer_dict()})
+        response_body.append(customer.customer_dict())
 #     for customer in customers:
 #         response_body.append({
 #             "id": customers.customer_id,
@@ -48,12 +56,13 @@ def handle_customers():
 @customers_bp.route("/<customer_id>", methods=["GET"])
 def customer_get(customer_id):
     customer = Customer.query.get(customer_id)
-    if customer == None:
-        return jsonify("Not Found", 404)
+    if customer is None:
+        return jsonify({"message": f"Customer {customer_id} was not found"}), 404
+        
 
-    return jsonify(customer.customer_dict), 200
+    return jsonify(customer.customer_dict()), 200
 
-
+# WORK IN PROGRESS
 @customers_bp.route("/<customer_id>", methods=["PUT"])
 def customer_put(customer_id):
     customer = Customer.query.get(customer_id)
@@ -66,10 +75,22 @@ def customer_put(customer_id):
     customer.name = request_body["name"]
     customer.phone = request_body["phone"]
     customer.postal_code = request_body["postal_code"]
-    # is registered not required?
-    response_body = {customer.customer_dict}
 
-    return jsonify(response_body), 200
+    
+    if "name" in request_body or "phone" in request_body or "postal_code" in request_body:
+        response_body ={}
+        if "name" in request_body:
+            response_body = f"Updated ${customer.name}"
+        elif "phone" in request_body:
+            response_body = f"Updated ${customer.phone}"
+        elif "postal_code" in request_body:
+            response_body = f"Updated ${customer.postal_code}"
+        return response_body
+    
+    
+    # response_body = customer.customer_dict()
+
+    # return jsonify(response_body), 200
 
 
 @customers_bp.route("/<customer_id>", methods=["DELETE"])
@@ -77,9 +98,9 @@ def customer_delete(customer_id):
     customer = Customer.query.get(customer_id)
 
     if customer == None:
-        return jsonify("Not Found", 404)
+        return jsonify({"message": f"Customer {customer_id} was not found"}), 404
 
     db.session.delete(customer)
     db.session.commit()
 
-    return jsonify({"id": customer.customer_id})
+    return jsonify({"id": customer.customer_id}), 200
--- app/models/customer.py
+++ app/models/customer.py
@@ -11,11 +11,15 @@ class Customer(db.Model):
     registered_at = db.Column(db.DateTime(timezone=True), server_default=func.now())
 
     def customer_dict(self):
-        return {
+        dict = {
             "id": self.customer_id,
             "name": self.name,
             "phone": self.phone,
-            "postal_code": self.postal_code,
+            "postal_code": str(self.postal_code),
             # weekday|day of month (16)|month name|year|local version of time|, UTC offset +HHMM or -HHMM
-            "registered_at": self.registered_at.strfttime("%a, %-d %b %Y %X %z")
+            "registered_at": self.registered_at.strftime("%a, %-d %b %Y %X %z")
     }
+    #     if self.registered_at is not None:
+    #         dict["registered_at"] = self.registered_at.strfttime("%a, %-d %b %Y %X %z")
+            
+        return dict
