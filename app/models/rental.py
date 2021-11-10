8fb18a5b22d719c6d0ec70ad8faba4bc27c0bccd
Kristel Jabbusch <kaj12@my.fsu.edu>
2021-11-09 19:02:03 -0800

    creating rental class and relationships

--- app/models/rental.py
+++ app/models/rental.py
@@ -1,5 +1,10 @@
 from app import db
+import datetime
 
 class Rental(db.Model):
     __tablename__ = 'rentals'
-    id = db.Column(db.Integer, primary_key=True)
+    rental_id = db.Column(db.Integer, primary_key=True)
+    customer_id = db.Column(db.Integer, db.ForeignKey('customers.customer_id'), nullable=False)
+    video_id = db.Column(db.Integer, db.ForeignKey('videos.video_id'), nullable=False)
+    # replace None with date week out
+    due_date = db.Column(db.DateTime, default=datetime.datetime.now() + datetime.timedelta(days=7), nullable=False)
