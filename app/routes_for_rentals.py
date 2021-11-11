e37706b7e954b75b8bea67113b6022e0f3da5f43
Emily Colon <emiquigs@gmail.com>
2021-11-12 12:17:04 +1300

    combined our check out function

--- app/routes_for_rentals.py
+++ app/routes_for_rentals.py
@@ -13,8 +13,6 @@ rentals_bp = Blueprint("rentals", __name__, url_prefix="/rentals")
 @rentals_bp.route("/check-out", methods=["POST"])
 def handle_rentals():
     request_body=request.get_json()
-    if not request_body.get("video_id") or not request_body.get("customer_id"):
-        return make_response({"message":"Could not perform checkout"}, 400)
     video = Video.query.get(request_body["video_id"])
     customer = Customer.query.get(request_body["customer_id"])
     if not customer:
@@ -23,18 +21,6 @@ def handle_rentals():
         return make_response({"message":"Could not perform checkout"}, 404)
 
 
-#I like what you did ^^^ so much cleaner than the messy way i have it set up now
-#I'm just so confused by the error and also how the hell to properly get the 
-#amount of rentals per customer and available rentals
-
-@rentals_bp.route("/check-out", methods=["POST"])
-def checkout_video():
-    request_body = request.get_json()
-    if not request_body.get("customer_id"):
-            return make_response({"details":"Request body must include customer id."}, 404) 
-    elif not request_body.get("video_id"):
-            return make_response({"details":"Request body must include video id."}, 404) 
-
     new_rental = Rental(
             customer_id=request_body["customer_id"],
             video_id=request_body["video_id"]
@@ -42,11 +28,9 @@ def checkout_video():
     db.session.add(new_rental)
     db.session.commit()
 
-    customer_checking_out = Customer.query.get(request_body["customer_id"])
-    rentals_per_customer = len(customer_checking_out.rentals)
-    
-    video_checking_out = Video.query.get(request_body["video_id"])
-    available_rentals_per_video= video_checking_out.available_rentals - rentals_per_customer
+
+    rentals_per_customer = len(customer.rentals)
+    available_rentals_per_video= video.available_rentals - rentals_per_customer
 
     response_value = {"customer_id":new_rental.customer_id,
         "video_id":new_rental.video_id,
