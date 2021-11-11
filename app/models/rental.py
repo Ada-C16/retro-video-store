16899d7a2e16f69d3c92fa0eed5183386f97f8a0
bailey <baileyholderman@gmail.com>
2021-11-11 13:16:14 -0800

    Adds PK to Rental model

--- app/models/rental.py
+++ app/models/rental.py
@@ -2,8 +2,9 @@ from app import db
 from datetime import timedelta, date
 
 class Rental(db.Model):
-    customer_id = db.Column(db.Integer, db.ForeignKey('customer.id'), primary_key=True,nullable=False)
-    video_id = db.Column(db.Integer, db.ForeignKey('video.id'), primary_key=True,nullable=False)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    customer_id = db.Column(db.Integer, db.ForeignKey('customer.id'),nullable=False)
+    video_id = db.Column(db.Integer, db.ForeignKey('video.id'),nullable=False)
     due_date = db.Column(db.DateTime, default=date.today() + timedelta(days=7))
     return_date = db.Column(db.DateTime, default=None)
 
