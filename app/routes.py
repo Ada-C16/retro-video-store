c775fe06826f06c53b4a3cf290d665cb0d1aebbb
Mariah Rosman <mrosman7@gmail.com>
2021-11-11 10:52:07 -0800

    refactors get and post methods for customer and video

--- app/routes.py
+++ app/routes.py
@@ -26,15 +26,29 @@ def validate_data(request_body, required_attributes):
     return request_body
 
 #Videos Routes
-@videos_bp.route("", methods=["GET"])
+@videos_bp.route("", methods=["GET", "POST"])
 def get_all_videos():
-    video_list = []
-    videos = Video.query.all()
+    if request.method == "GET":
+        video_list = []
+        videos = Video.query.all()
 
-    for video in videos:
-        video_list.append(video.to_dict())
-    
-    return make_response(jsonify(video_list), 200)
+        for video in videos:
+            video_list.append(video.to_dict())
+        
+        return make_response(jsonify(video_list), 200)
+
+    elif request.method == "POST":
+        required_attributes = ["title", "release_date", "total_inventory"]
+        request_body = validate_data(request.get_json(), required_attributes)
+
+        new_video = Video(title = request_body["title"],
+                        release_date = request_body["release_date"],
+                        total_inventory = request_body["total_inventory"]
+                        )
+        db.session.add(new_video)
+        db.session.commit()
+
+        return make_response(jsonify(new_video.to_dict()), 201)
 
 @videos_bp.route("/<video_id>", methods=["GET"])
 def get_one_video(video_id):
@@ -65,21 +79,6 @@ def delete_one_video(video_id):
             db.session.commit()
             return make_response("", 200)
 
-
-@videos_bp.route("", methods=["POST"])
-def create_new_video():
-    required_attributes = ["title", "release_date", "total_inventory"]
-    request_body = validate_data(request.get_json(), required_attributes)
-
-    new_video = Video(title = request_body["title"],
-                    release_date = request_body["release_date"],
-                    total_inventory = request_body["total_inventory"]    
-)
-    db.session.add(new_video)
-    db.session.commit()
-
-    return make_response(jsonify(new_video.to_dict()), 201)
-
 @videos_bp.route("/<video_id>", methods=["PUT"])
 def update_video(video_id):
     video = Video.query.get(video_id)
@@ -99,32 +98,32 @@ def update_video(video_id):
 
 
 # Customer Routes
-@customers_bp.route("", methods=["POST"])
-def create_customer():
-    required_attributes = ["postal_code", "name", "phone"]
-    request_body = validate_data(request.get_json(), required_attributes)
+@customers_bp.route("", methods=["GET", "POST"])
+def handle_customers():
+    if request.method == "GET":
+        customers_list = []
+        customers = Customer.query.all()
 
-    new_customer = Customer(
-        name = request_body["name"],
-        postal_code = request_body["postal_code"],
-        phone = request_body["phone"],
-        register_at = date.today()
-        )
+        for customer in customers:
+            customers_list.append(customer.to_dict())
 
-    db.session.add(new_customer)
-    db.session.commit()
+        return make_response(jsonify(customers_list), 200)
 
-    return make_response(new_customer.to_dict(), 201)
+    elif request.method == "POST":
+        required_attributes = ["postal_code", "name", "phone"]
+        request_body = validate_data(request.get_json(), required_attributes)
 
-@customers_bp.route("", methods=["GET"])
-def get_all_customers():
-    customers_list = []
-    customers = Customer.query.all()
+        new_customer = Customer(
+            name = request_body["name"],
+            postal_code = request_body["postal_code"],
+            phone = request_body["phone"],
+            register_at = date.today()
+            )
 
-    for customer in customers:
-        customers_list.append(customer.to_dict())
+        db.session.add(new_customer)
+        db.session.commit()
 
-    return make_response(jsonify(customers_list), 200)
+        return make_response(new_customer.to_dict(), 201)
 
 @customers_bp.route("/<customer_id>", methods=["GET", "DELETE", "PUT"])
 def handle_one_customer(customer_id):
