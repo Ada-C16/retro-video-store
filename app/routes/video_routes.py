63b5d4d5a53337754d32df72abd2e9fbd4b99f1d
Laurel S <ls60@protonmail.com>
2021-11-10 20:07:02 -0800

    Add GET '/<id>/rentals' endpoint, passes relevant tests

--- app/routes/video_routes.py
+++ app/routes/video_routes.py
@@ -1,7 +1,8 @@
-from flask import Blueprint, jsonify, request, abort
-from flask.helpers import make_response
-from app.models.video import Video
 from app import db
+from flask import Blueprint, jsonify, request, abort, make_response
+from app.models.video import Video
+from app.models.rental import Rental
+from app.models.customer import Customer
 from sqlalchemy.exc import DataError
 
 # helper functions
@@ -76,9 +77,27 @@ def update_video(video_id):
         return make_response({'error':'Invalid data type in request body'}, 400)
     return make_response(video.to_dict(), 200)
 
-@videos_bp.route('<video_id>', methods=['DELETE'], strict_slashes=False)
+@videos_bp.route('/<video_id>', methods=['DELETE'], strict_slashes=False)
 def delete_video(video_id):
     video = get_video_from_id(video_id)
     db.session.delete(video)
     db.session.commit()
     return make_response(video.to_dict(), 200)
+
+# Rental route
+@videos_bp.route('/<id>/rentals', methods=['GET'], strict_slashes=False)
+def get_customers_currently_with_video(id):
+    # check if video exists
+    get_video_from_id(id)
+    rental_record = Rental.query.filter_by(video_id=id, checked_in=False).all()
+    record_list = [record for record in rental_record]
+    response = []
+    for record in record_list:
+        customer = Customer.query.get(record.customer_id)
+        response.append({
+            "due_date": record.due_date,
+            "name": customer.name,
+            "phone": customer.phone,
+            "postal_code": customer.postal_code,
+            })
+    return make_response(jsonify(response), 200)
