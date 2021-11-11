2d1fe43d02c747cd88ebb9efabd2f369dbdecc91
Nicole Washington <nicolewashington.003@gmail.com>
2021-11-11 13:31:32 -0800

    Seperated CRUD into seperate functions

--- app/video_routes.py
+++ app/video_routes.py
@@ -7,13 +7,14 @@ import os
 
 video_bp = Blueprint("videos", __name__, url_prefix="/videos")
 
-@video_bp.route("", methods = ["GET","POST"])
-def handle_videos():
-    if request.method == "GET":
+@video_bp.route("", methods = ["GET"])
+def get_videos():
         videos = Video.query.all()
         response_body = build_videos_response(videos)
         return jsonify(response_body),200
 
+@video_bp.route("", methods = ["POST"])
+def post_video():
     if request.method == "POST":
         request_body = request.get_json()
         valid_request = "title" in request_body and "total_inventory"\
@@ -34,8 +35,8 @@ def handle_videos():
             return jsonify(response_body), 201
 
 
-@video_bp.route("<video_id>", methods = ["GET","PUT","DELETE"])
-def handle_video (video_id): 
+@video_bp.route("<video_id>", methods = ["GET"])
+def get_video (video_id): 
     # have to check before query otherwise get a data error
     if not video_id.isnumeric():
         return jsonify({"details": "id must be numerical"}),400
@@ -43,36 +44,47 @@ def handle_video (video_id):
     video = Video.query.get(video_id)
     if video == None:
         return jsonify({"message": f"Video {video_id} was not found"}), 404
+
+    response_body = build_video_response(video)
+    return jsonify(response_body), 200
+
+@video_bp.route("<video_id>", methods = ["PUT"])
+def put_video(video_id):
+
+    if not video_id.isnumeric():
+        return jsonify({"details": "id must be numerical"}),400
     
-    #method request#
-    #GET
-    if request.method == "GET":
-        response_body = build_video_response(video)
-        return jsonify(response_body), 200
+    video = Video.query.get(video_id)
+    if video == None:
+        return jsonify({"message": f"Video {video_id} was not found"}), 404
 
-    #PUT
-    if request.method == "PUT":
-        request_body = request.get_json()
-        valid_request = "title" in request_body and "total_inventory" in request_body and \
-            "release_date" in request_body
-        if not valid_request:
-            response = build_invalid_post_response(request_body)
-            return response
-        else:
-            video.title = request_body["title"],
-            video.release_date = request_body["release_date"],
-            video.total_inventory = request_body["total_inventory"]
+    request_body = request.get_json()
+    valid_request = "title" in request_body and "total_inventory" in request_body and \
+        "release_date" in request_body
+    if not valid_request:
+        response = build_invalid_post_response(request_body)
+        return response
+    else:
+        video.title = request_body["title"],
+        video.release_date = request_body["release_date"],
+        video.total_inventory = request_body["total_inventory"]
             
-            db.session.commit()
-            response_body = build_video_response(video)
-            return jsonify(response_body), 200
-
-    #DELETE
-    if request.method == "DELETE":
-        db.session.delete(video)
         db.session.commit()
         response_body = build_video_response(video)
         return jsonify(response_body), 200
+
+
+@video_bp.route("<video_id>", methods = ["DELETE"])
+def delete_video(video_id):
+    if not video_id.isnumeric():
+        return jsonify({"details": "id must be numerical"}),400
+    video = Video.query.get(video_id)
+    if video == None:
+        return jsonify({"message": f"Video {video_id} was not found"}), 404
+    db.session.delete(video)
+    db.session.commit()
+    response_body = build_video_response(video)
+    return jsonify(response_body), 200
 
 ##HELP##
 def build_videos_response(videos):
