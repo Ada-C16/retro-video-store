b5de22719fbd4c89ad60cf89cc10b5a169d2d6de
Rhyannon <rhyannonjoy@gmail.com>
2021-11-11 10:12:39 -0700

    Added Video Model, Passes Wave 1 Video Tests

--- app/models/customer.py
+++ app/models/customer.py
@@ -2,3 +2,16 @@ from app import db
 
 class Customer(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    postal_code = db.Column(db.Integer)
+    phone_number = db.Column(db.String)
+    register_at = db.Column(db.DateTime, nullable=True)
+
+    def to_json(self):
+        return {
+                "id": self.id,
+                "name": self.name,
+                "postal_code": self.postal_code,
+                "phone_number": self.phone_number,
+                "register_at": False if self.completed_at == None else True
+                }
