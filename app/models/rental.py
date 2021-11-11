a55248d03cb5f4e9563802e49ad4107c55fedf33
Zandra Nguyen <zandra.eng@gmail.com>
2021-11-11 12:00:03 -0800

    add rentals_checkout function. of course more garbage code.

--- app/models/rental.py
+++ app/models/rental.py
@@ -5,4 +5,6 @@ class Rental(db.Model):
     rental_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     customer_id = db.Column(db.Integer, db.ForeignKey('customer.customer_id'), primary_key=True,nullable=False)
     video_id = db.Column(db.Integer, db.ForeignKey('video.video_id'), primary_key=True,nullable=False)
-    due_date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow() + datetime.timedelta(days = 7))
+    due_date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow() + timedelta(days = 7))
+    #video = db.relationship("Video", back_populates="customers")
+    #customer = db.releationship("Customer", back_populates="videos")
--- app/rental_routes.py
+++ app/rental_routes.py
@@ -1,6 +1,6 @@
 from app import db
-from customer_routes import customers_bp
-from video_routes import video_bp
+from app.customer_routes import customers_bp
+from app.video_routes import video_bp
 from app.models.customer import Customer
 from app.models.video import Video
 from app.models.rental import Rental
@@ -11,17 +11,56 @@ rentals_bp = Blueprint("rentals_bp", __name__, url_prefix="/rentals")
 
 @rentals_bp.route("/check-out", methods=["POST"])
 def rentals_checkout():
-    request_body = request.get_sjson()
-    """
-    get request body from client
-    check to make sure required attributes are in request body
-    Instantiate a new instance for rental
-    ?customer.videos or video.customers?
-    add rental instance to database
-    commit to database
-    return the response body and status code
+    #get request body from client
+    #{"c_id": [1], "v_id": [4]}
+    request_body = request.get_json() 
+
+    customer = Customer.query.get(request_body["customer_id"])
+    if customer is None:
+        return jsonify(), 404
+
+    video = Customer.query.get(request_body["video_id"])
+    if video is None:
+        return jsonify(), 404
+
+    # another if statement for video
+
+    # check to make sure required attributes are in request body
+    if "customer_id" not in request_body or "video_id" not in request_body:
+        return jsonify(), 400
     
-    """
+    
+    # ?customer.videos or video.customers?
+    # customer_ids = []
+    # for customer_id in request_body["customer_id"]:
+    #     customer_ids.append(Customer.query.get(customer_id))
+    
+        
+    # video_ids = []
+    # for video_id in request_body["video_id"]:
+    #     video_ids.append(Video.query.get(video_id))
+
+    
+    # Instantiate a new instance for rental
+    new_rental = Rental(
+        video_id=video.video_id,
+        customer_id=customer.customer_id
+    )  
+    
+    # add rental instance to database
+    db.session.add(new_rental)
+    # commit to database
+    db.session.commit()
+    
+    # return the response body and status code
+    return jsonify({
+        "video_id": new_rental.video_id,
+        "customer_id": new_rental.cusomer_id,
+        "videos_checked_out_count": len(customer.videos),
+        "available_inventory": video.total_inventory - len(video.customers)
+    }), 200
+
+
 
 @rentals_bp.route("/check-in", methods=["POST"])
 def rentals_checkin():
@@ -33,4 +72,7 @@ def customer_read():
 
 @video_bp.route("/<video_id>/rentals", methods=["GET"])
 def video_read():
+    # videos_list = []
+    # for video in self.videos:
+    #     videos_list(video.rental_id)
     pass
