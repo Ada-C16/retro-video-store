150530cef8059fef401756826713a4671952ce55
Stephanie <stephanie.chenn1@gmail.com>
2021-11-10 21:36:49 -0500

    created Rental model

--- app/models/rental.py
+++ app/models/rental.py
@@ -2,3 +2,8 @@ from app import db
 
 class Rental(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    customer_id = db.Column(db.Integer, db.ForeignKey('customer.id'))
+    video_id = db.Column(db.Integer, db.ForeignKey('video.id'))
+    due_date = db.Column(db.DateTime)
+    videos_checked_out_count = db.Column(db.Integer)
+    available_inventory = db.Column(db.Integer)
