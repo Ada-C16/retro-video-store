f4b07cc4a3103ab5519ff159f47d94f254ba2a3c
Zandra Nguyen <zandra.eng@gmail.com>
2021-11-11 12:33:12 -0800

    add checkout route. need to pass 1 more test

--- app/rental_routes.py
+++ app/rental_routes.py
@@ -11,36 +11,23 @@ rentals_bp = Blueprint("rentals_bp", __name__, url_prefix="/rentals")
 
 @rentals_bp.route("/check-out", methods=["POST"])
 def rentals_checkout():
-    #get request body from client
     #{"c_id": [1], "v_id": [4]}
     request_body = request.get_json() 
+
+    if "customer_id" not in request_body or "video_id" not in request_body:
+        return jsonify(), 400
 
     customer = Customer.query.get(request_body["customer_id"])
     if customer is None:
         return jsonify(), 404
 
-    video = Customer.query.get(request_body["video_id"])
+    video = Video.query.get(request_body["video_id"])
     if video is None:
         return jsonify(), 404
 
-    # another if statement for video
+    if video.total_inventory == 0:
+        return jsonify({"message": "Could not perform checkout"}), 400
 
-    # check to make sure required attributes are in request body
-    if "customer_id" not in request_body or "video_id" not in request_body:
-        return jsonify(), 400
-    
-    
-    # ?customer.videos or video.customers?
-    # customer_ids = []
-    # for customer_id in request_body["customer_id"]:
-    #     customer_ids.append(Customer.query.get(customer_id))
-    
-        
-    # video_ids = []
-    # for video_id in request_body["video_id"]:
-    #     video_ids.append(Video.query.get(video_id))
-
-    
     # Instantiate a new instance for rental
     new_rental = Rental(
         video_id=video.video_id,
@@ -55,7 +42,7 @@ def rentals_checkout():
     # return the response body and status code
     return jsonify({
         "video_id": new_rental.video_id,
-        "customer_id": new_rental.cusomer_id,
+        "customer_id": new_rental.customer_id,
         "videos_checked_out_count": len(customer.videos),
         "available_inventory": video.total_inventory - len(video.customers)
     }), 200
