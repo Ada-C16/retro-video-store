8f7d2acd8d0a6afff69a1b50e4fef97608f197df
Ngozi Amaefule <ngoziamaefule1@gmail.com>
2021-11-12 11:12:17 -0500

    Updated Rentals model and established relationships between models

--- app/models/rental.py
+++ app/models/rental.py
@@ -1,4 +1,17 @@
+from sqlalchemy.orm import backref
 from app import db
+from datetime import datetime, timedelta
 
+def week_later():
+    return datetime.utcnow() + timedelta(days=7)
+    
 class Rental(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    customer_id = db.Column(db.Integer, db.ForeignKey('customer.id'),  nullable=False)
+    customer = db.relationship('Customer', backref=db.backref('rentals', lazy=True))
+    video_id = db.Column(db.Integer, db.ForeignKey('video.id'), nullable=False)
+    video = db.relationship('Video', backref=db.backref('rentals', lazy=True))
+    due_date = db.Column(db.DateTime, nullable=False, default=week_later)
+
+# Because the remaining two columns are dynamic, it would be easier to maintain in routes.py
+# We could add them as methods in the video model.
--- app/models/video.py
+++ app/models/video.py
@@ -1,7 +1,24 @@
 from app import db
+from app.models.rental import Rental
 
 class Video(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String)
     release_date = db.Column(db.DateTime)
     total_inventory = db.Column(db.Integer)
+    
+    def videos_checked_out_count(self):
+        """
+        We are creating this method here because this is where 
+        we have the information about the inventory and with the back ref
+        we have access to the rental information.
+        """ 
+        rental_query = Rental.query.filter_by(video_id=self.id) 
+        return rental_query.count() # Here we created a query that we were able to take a count of.
+
+# Don't forget that we only want to count active rentals. 
+# Right now, in Wave 2, they haven't required us to provide information about that.
+
+    def available_inventory(self):
+        available_inv = self.total_inventory - self.videos_checked_out_count()
+        return available_inv
