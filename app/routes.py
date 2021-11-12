41f9399e3a8f7fb05284c74e6abe22c451eeedbb
mac-madison <macanmadison@gmail.com>
2021-11-12 11:44:51 -0800

    delete routes.py

--- app/routes.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, jsonify, request, abort, make_response
-from app import db
-from app.models.video import Video
-from app.models.customer import Customer
-from app.validate import Validate
-
-videos_bp = Blueprint("videos", __name__, url_prefix="/videos")
-customer_bp = Blueprint("customers", __name__, url_prefix="/customers")
