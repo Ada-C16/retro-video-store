893fba1098c73d2c63cd84b2c363954b59b55fd7
Faith <fkauwe@gmail.com>
2021-11-11 17:00:49 -0800

    created Rental Model and established many to many relationships with 3 tables/ Models

--- app/models/customer.py
+++ app/models/customer.py
@@ -1,5 +1,6 @@
 from app import db
 from flask import current_app
+from sqlalchemy.orm import backref
 
 
 class Customer(db.Model):
@@ -8,3 +9,7 @@ class Customer(db.Model):
     postal_code = db.Column(db.String)
     phone = db.Column(db.String)
     registered_at = db.Column(db.DateTime)
+    videos_checked_out_count = db.Column(db.Integer)
+# backref is creating 2 new attributes as well as establishing the relationship here
+# backref is creating Customer.videos as well as Video.customers    
+    videos = db.relationship("Video", secondary="rental", backref="customers")
--- app/models/rental.py
+++ app/models/rental.py
@@ -1,4 +1,9 @@
 from app import db
+from sqlalchemy.orm import backref
 
 class Rental(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    customer_id = db.Column(db.Integer, db.ForeignKey('customer.id'), primary_key=True, nullable=False)
+    video_id = db.Column(db.Integer, db.ForeignKey('video.id'), primary_key=True, nullable=False)
+    checked_in= db.Column(db.Boolean)
+    due_date = db.Column(db.DateTime)
