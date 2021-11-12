b400e27f60395767b2517f080b05a862dfff8c02
Arianna Cabebe <aricabebe@gmail.com>
2021-11-11 20:34:42 -0800

    adds POST check-in and GET rentals by customer routes. 16/20 wave 2 tests passing.

--- app/rental_routes.py
+++ app/rental_routes.py
@@ -25,9 +25,10 @@ def rentals_checkout():
     video = Video.query.get(request_body["video_id"])
     if video is None:
         return jsonify(), 404
-
-    if video.total_inventory == 0:
-        return jsonify({"message": "Could not perform checkout"}), 400
+    
+    # # revisit later
+    # if video.total_inventory == 0:
+    #     return jsonify({"message": "Could not perform checkout"}), 400
 
     # Instantiate a new instance for rental
     new_rental = Rental(
@@ -48,20 +49,83 @@ def rentals_checkout():
         "available_inventory": video.total_inventory - len(video.customers)
     }), 200
 
+@rentals_bp.route("/check-in", methods=["POST"])
+def rentals_checkin():
+    request_body = request.get_json()
+    ## revisit later
+    # if video and customer do not match a current rental, return 400
 
+    # checks that the required request body parameters are in request 
+    if "customer_id" not in request_body or "video_id" not in request_body:
+        return jsonify(), 400
+    
+    # stores customer instance into variable. If customer does not exist, returns 404 
+    customer = Customer.query.get(request_body["customer_id"])
+    if customer is None:
+        return jsonify(), 404
 
-#     # response body might look something like this: 
-#     { 
-#     "customer_id": rental.customer_id,
-#     "video_id": rental.video_id,
-#     "videos_checked_out_count": rental.customer.videos,
-#     "available_inventory": video.total_inventory - video.customers
-#     }
+    # stores video instance into variable. If video does not exist, returns 404 
+    video = Video.query.get(request_body["video_id"])
+    if video is None:
+        return jsonify(), 404
+    
+    # iterate through all rentals instances to find rental with matching customer and video ids
+    rentals = Rental.query.all()
+    checked_in = None
+    for rental in rentals:
+        if rental.customer_id == request_body["customer_id"] and rental.video_id == request_body["video_id"]:
+            checked_in = rental
+    
+    # if there is no matching rental for the given customer and video ids, return 400
+    if checked_in is None:
+        return jsonify({"message": "No outstanding rentals for customer 1 and video 1"}), 400
+    
+    # delete rental that is being checked in
+    db.session.delete(checked_in)
+    # commit to database
+    db.session.commit()
+
+    # return response body
+    return jsonify({
+        "video_id": checked_in.video_id,
+        "customer_id": checked_in.customer_id,
+        "videos_checked_out_count": len(customer.videos),
+        "available_inventory": video.total_inventory - len(video.customers)
+    }), 200
 
 @customers_bp.route("/<customer_id>/rentals", methods=["GET"])
 def customer_read(customer_id):
     """ List the videos a customer currently has checked out """
-    pass
+    request_body = request.get_json()
+    
+    # checks to see if customer exists. If not, returns 404
+    customer = Customer.query.get(customer_id)
+    if customer is None:
+        return jsonify({"message": f"Customer {customer_id} was not found"}), 404
+    
+    # sets up empty list to store a customer's checked out videos
+    #  iterates through customer.videos to retreive all videos a customer has
+    checked_out = []
+    for video in customer.videos:
+        checked_out.append(video)
+
+    # gets rental instance for each video a customer has
+
+    rentals = Rental.query.all()
+    customer_rentals = []
+    for video in checked_out:
+        for rental in rentals:
+            if video.video_id == rental.video_id and customer.customer_id == rental.customer_id:
+                customer_rentals.append(rental)
+
+    response_body = []
+    for rental in customer_rentals:
+        response_body.append({
+            "release_date": video.release_date,
+            "title": video.title,
+            "due_date": rental.due_date
+        })
+    return jsonify(response_body)
 
 @video_bp.route("/<video_id>/rentals", methods=["GET"])
 def video_read(video_id):
