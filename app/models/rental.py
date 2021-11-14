735852d52a254d29b0e2a472c12b306f82b8de4c
chrisss111 <christinecoding10@gmail.com>
2021-11-14 13:41:46 -0500

    fixed typo in rental model

--- app/models/rental.py
+++ app/models/rental.py
@@ -5,7 +5,7 @@ class Rental(db.Model):
     __tablename__ = "rentals"
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     customer_id = db.Column(db.Integer, db.ForeignKey('customer.id'), primary_key=True, nullable=False)
-    video_id = db.Column(db.Integer, db.ForeignKey('video.id'), primary_ket=True, nullable=False)
+    video_id = db.Column(db.Integer, db.ForeignKey('video.id'), primary_key=True, nullable=False)
     due_date = db.Column(db.DateTime)
     checked_in = db.Column(db.Boolean, default=False)
 
