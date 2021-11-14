07ce7f116fb815f1c2a733198e1efc149b6a70fe
karishma <karishmacarterjohnson@gmail.com>
2021-11-14 10:01:01 -0800

    add checkout_date column to model, define customer history fxn

--- app/models/rental.py
+++ app/models/rental.py
@@ -7,6 +7,7 @@ class Rental(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     customer = db.Column('customer_id', db.Integer, db.ForeignKey('customer.id', ondelete = 'cascade'))
     video = db.Column('video_id', db.Integer, db.ForeignKey('video.id', ondelete = 'cascade'))
+    checkout_date = db.Column(db.DateTime, default = dt.datetime.now())
     due_date = db.Column(db.DateTime, default = dt.datetime.now() + dt.timedelta(days = 7))
     videos_checked_out_count = db.Column(db.Integer)
     available_inventory = db.Column(db.Integer)
@@ -27,3 +28,14 @@ def query_customers_videos(customer_id):
 def count_a_videos_inventory(video_id, video_inventory):
     checked_out = Video.query.join(Rental).filter(Video.id == video_id, Rental.checked_in == False).all()
     return video_inventory - len(checked_out)
+
+def customer_history(customer_id):
+    rentals = Rental.query.join(Customer).join(Video).filter(Customer.id == customer_id).add_columns(Video.title, Rental.checkout_date, Rental.due_date).all()
+    rental_history = []
+    for rental in rentals:
+        rental_history.append({
+            "title": rental.title,
+            "checkout_date": rental.checkout_date,
+            "due_date": rental.due_date
+        })
+    return rental_history
