65f8a5c5f84aa1ad939505a42f1da1504cafc668
sjolivas <sjolivas@gmail.com>
2021-11-14 20:26:56 -0600

    Added unique constraint to customer_id in rental

--- app/models/rental.py
+++ app/models/rental.py
@@ -9,7 +9,7 @@ class Rental(db.Model):
     # __tablename__ = "rentals"
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     video_id = db.Column(db.Integer, db.ForeignKey('video.id'), nullable=True)
-    customer_id = db.Column(db.Integer, db.ForeignKey('customer.customer_id'), nullable=False)
+    customer_id = db.Column(db.Integer, db.ForeignKey('customer.customer_id'), unique=True, nullable=False)
     due_date = db.Column(db.DateTime, nullable=False, default=datetime.datetime.utcnow() + datetime.timedelta(days=7))
 
     def rental_dict(self):
