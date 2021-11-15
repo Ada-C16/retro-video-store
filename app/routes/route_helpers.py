32b243cfd5d6d9b276a3cf4ce8af5ab8e6c3af15
Sophia Le <sophiale@sophias-mbp.lan>
2021-11-15 13:18:18 -0800

    Adds response_body validation helper

--- app/routes/route_helpers.py
+++ app/routes/route_helpers.py
@@ -6,3 +6,12 @@ def invalid_id(id):
             }
             return make_response(jsonify(response_body), 400)
     return None
+
+def invalid_request_body(request_body, required_keys):
+    for key in required_keys:
+        if key not in request_body:
+            response_body = {
+                'details': f'Request body must include {key}.'
+            }
+            return make_response(response_body, 400)
+    return None
--- app/routes/video_routes.py
+++ app/routes/video_routes.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, jsonify, make_response, request, abort
 from app import db
 from app.models.video import Video, video_from_json
-from app.routes.route_helpers import invalid_id
+from app.routes.route_helpers import invalid_id, invalid_request_body
 
 video_bp = Blueprint('videos', __name__, url_prefix='/videos')
 
@@ -14,37 +14,19 @@ def read_all_videos():
 @video_bp.route('', methods=['POST'])
 def create_new_video():
     request_body = request.get_json()
-    if 'title' not in request_body:
-        response_body = {
-            'details': 'Request body must include title.'
-        }
-        return make_response(jsonify(response_body), 400)
-    if 'release_date' not in request_body:
-        response_body = {
-            'details': 'Request body must include release_date.'
-        }
-        return make_response(jsonify(response_body), 400)
-    if 'total_inventory' not in request_body:
-        response_body = {
-            'details': 'Request body must include total_inventory.'
-        }
-        return make_response(jsonify(response_body), 400)
-    #try:
+    required_keys = ['title', 'release_date', 'total_inventory']
+    if invalid_request_body(request_body, required_keys):
+        return invalid_request_body(request_body, required_keys)
+
     video_to_create = video_from_json(request_body)
     db.session.add(video_to_create)
     db.session.commit()
 
     response_body = video_to_create.to_dict()
     return make_response(jsonify(response_body), 201)
-    #except TypeError:
 
 @video_bp.route('/<video_id>', methods=['GET'])
 def read_single_video(video_id):
-    # if not video_id.isnumeric():
-    #     response_body = {
-    #         'details': 'Invalid video id'
-    #     }
-    #     return make_response(jsonify(response_body), 400)
     if invalid_id(video_id):
         return invalid_id(video_id)
 
@@ -58,10 +40,6 @@ def read_single_video(video_id):
 
 @video_bp.route("/<video_id>/rentals", methods=["GET"])
 def read_one_video_rentals(video_id):
-    # try:
-    #     int(video_id)
-    # except:
-    #     return {"message": "Invalid data"}, 400
     if invalid_id(video_id):
         return invalid_id(video_id)
 
@@ -76,32 +54,15 @@ def read_one_video_rentals(video_id):
 
 @video_bp.route('/<video_id>', methods=['PUT'])
 def update_single_video(video_id):
-    # if not video_id.isnumeric():
-    #     response_body = {
-    #         'details': 'Invalid video id'
-    #     }
-    #     return make_response(jsonify(response_body), 400)
     if invalid_id(video_id):
         return invalid_id(video_id)
 
     video_to_update = Video.query.get(video_id)
     try:
         request_body = request.get_json()
-        if 'title' not in request_body:
-            response_body = {
-            'details': 'Request body must include title.'
-            }
-            return make_response(jsonify(response_body), 400)
-        if 'release_date' not in request_body:
-            response_body = {
-                'details': 'Request body must include release_date.'
-            }
-            return make_response(jsonify(response_body), 400)
-        if 'total_inventory' not in request_body:
-            response_body = {
-                'details': 'Request body must include total_inventory.'
-            }
-            return make_response(jsonify(response_body), 400)
+        required_keys = ['title', 'release_date', 'total_inventory']
+        if invalid_request_body(request_body, required_keys):
+            return invalid_request_body(request_body, required_keys)
         
         video_to_update.title = request_body['title']
         video_to_update.release_date = request_body['release_date']
@@ -115,11 +76,6 @@ def update_single_video(video_id):
 
 @video_bp.route('/<video_id>', methods=['DELETE'])
 def delete_single_video(video_id):
-    # if not video_id.isnumeric():
-    #     response_body = {
-    #         'details': 'Invalid video id'
-    #     }
-    #     return make_response(jsonify(response_body), 400)
     if invalid_id(video_id):
         return invalid_id(video_id)
 
