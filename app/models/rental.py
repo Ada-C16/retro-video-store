ff5b5d7be2db1b7dbf60935f3b78673e0a1bade7
kristina <kkraft423@gmail.com>
2021-11-14 20:45:14 -0500

    changed 'lazy' config

--- app/models/rental.py
+++ app/models/rental.py
@@ -3,10 +3,6 @@ from sqlalchemy.orm import backref
 
 class Rental(db.Model):
 
-    __mapper_args__ = {
-        'confirm_deleted_rows': False
-    }
-    
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     customer_id = db.Column(db.Integer, db.ForeignKey('customer.id'), primary_key=True, nullable=False)
     video_id = db.Column(db.Integer, db.ForeignKey('video.id'), primary_key=True, nullable=False)
@@ -16,6 +12,6 @@ class Rental(db.Model):
     # updated video and customer with cascade
     # When the customer or video is deleted, all associated rentals are also deleted
     # cascade allows us to "delete the ophans" associated with the video or customer id 
-    video =db.relationship('Video', backref=backref('rentals', cascade="all, delete-orphan"))
+    video =db.relationship('Video', backref=backref('rentals', cascade="all, delete-orphan", lazy="joined"))
     # this relationship establishes a new attribute for each rental object, which allows it to access the Customer object
-    customer =db.relationship('Customer', backref=backref('rentals', cascade="all, delete-orphan"))
+    customer =db.relationship('Customer', backref=backref('rentals', cascade="all, delete-orphan", lazy="joined"))
