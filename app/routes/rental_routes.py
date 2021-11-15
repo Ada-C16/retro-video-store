0ee73dedc280304cd857d0e2d8d0571eab6b301e
goblinbrooke <BrookeVlahos@gmail.com>
2021-11-14 18:02:49 -0800

    Checkin

--- app/routes/rental_routes.py
+++ app/routes/rental_routes.py
@@ -36,6 +36,7 @@ def checkout_dict(video, customer):
         "available_inventory": video.get_available_inventory()
     }
 
+
 # Nested Route for Rentals - Post checkout
 @rental_bp.route("/check-out", methods=["POST"])
 def create_rental():
@@ -69,13 +70,23 @@ def create_rental():
 @rental_bp.route("/check-in", methods=["POST"])
 def checkin_video():
     request_body = request.get_json()
+    if "customer_id" not in request_body or "video_id" not in request_body:
+        return jsonify({"Error": "Missing Customer ID or Video ID."}), 400
+    
+    customer_id = request_body["customer_id"]
+    customer = get_object_with_id("customer", customer_id)
     video_id = request_body["video_id"]
-    video = Video.query.get(video_id)
-    if video is None:
-        return jsonify({"message": f"Video {video_id} was not found"}), 404
-    elif video.total_inventory == 0:
-        return jsonify({"message": f"Video {video_id} is out of stock"}), 404
-    else:
-        video.available_inventory = video.available_inventory + 1
-        db.session.commit()
-        return jsonify(video.to_dict()), 200
+    video = get_object_with_id("video", video_id)
+
+    try:
+        customer_id = int(customer_id)
+        video_id = int(video_id)
+    except ValueError:
+        return jsonify({"Error": "Customer ID and Video ID must be integers."}), 400
+    
+    rental = Rental.query.filter_by(customer_id=customer_id, video_id=video_id).first()
+    if rental is None:
+        return jsonify({"message": f"No outstanding rentals for customer {customer_id} and video {video_id}"}), 400
+    db.session.delete(rental)
+    db.session.commit()
+    return jsonify(checkout_dict(video, customer)), 200
